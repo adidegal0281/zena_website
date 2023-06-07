43e7e873ed5cd5d4adba0b5acbf44192dd362a6b
adidegal0281 <79826018+adidegal0281@users.noreply.github.com>
2023-06-07 11:18:08 +0530

    Update zena_app.py

--- zena_app.py
+++ zena_app.py
@@ -2,8 +2,8 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-streamlit.title('Zena Amazing Athleisure Catalog')
 from urllib.error import URLError
+streamlit.title('Zena Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
