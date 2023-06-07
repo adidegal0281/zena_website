7cd59814f9b6571f99821a74577c91f8891c19bb
adidegal0281 <79826018+adidegal0281@users.noreply.github.com>
2023-06-07 11:08:16 +0530

    Update zena_app.py

--- zena_app.py
+++ zena_app.py
@@ -4,3 +4,11 @@ import requests
 import snowflake.connector
 streamlit.title('Zena Amazing Athleisure Catalog')
 from urllib.error import URLError
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from zenas_athleisure_db.products.catalog_for_website;")
+    return my_cur.fetchall()
+my_data_row = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_row)
+  
