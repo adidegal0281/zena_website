c24f9b8d406163a381ed2ec5f06cb82593b42990
adidegal0281 <79826018+adidegal0281@users.noreply.github.com>
2023-06-08 17:57:14 +0530

    Update zena_app.py

--- zena_app.py
+++ zena_app.py
@@ -2,6 +2,6 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 streamlit.title('Zena Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
